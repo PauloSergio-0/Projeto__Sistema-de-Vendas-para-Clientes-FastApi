d3238a7fe23f6a2945ba4f5146d7293b291971ee
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-10-21 14:28:10 -0300

    adicionado as rotas para conexão de com o flask

--- src/Service/api_flask.py
+++ src/Service/api_flask.py
@@ -6,8 +6,9 @@ class APIFlask:
         self.produto = Config.URL_PRODUTO
         self.venda = Config.URL_VENDA
         
-        
-    def send_data(self, data, type_data):
+    # será enviado o json  para a url de acordo com o tipo de dados (cliente, produto  ou venda)
+    
+    def send_data(self, data, type_data): 
         
         if type_data == "cliente":            
             url = self.cliente
--- src/settings/config.py
+++ src/settings/config.py
@@ -1,4 +1,6 @@
 class Config:
+
+    # rotas F[do flask
     URL_CLIENTE = "http://127.0.0.1:5000/import_data/cliente"
     URL_PRODUTO = "http://127.0.0.1:5000/import_data/produto"
     URL_VENDA = "http://127.0.0.1:5000/import_data/venda"
