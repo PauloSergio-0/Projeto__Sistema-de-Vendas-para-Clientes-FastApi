aa1c56785a1f395f1ccc8e999d212620cba1a4ac
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-11-06 19:35:29 -0300

    removed data functions

--- src/data/__init__.py
+++
@@ -1,1 +0,0 @@
-from .data_memo import Data_Memory
--- src/data/data_memo.py
+++
@@ -1,5 +0,0 @@
-class Data_Memory:
-    def __init__(self):
-        self.clientes = None
-        self.produtos = None
-        self.vendas = None
